480c5bc35e7e849fe4b934411a6a9fde327a75fe
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2024-04-04 21:14:19 +0900

    [#2] feat: mocks.controller.ts

--- src/mocks/mocks.controller.ts
+++ src/mocks/mocks.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Get, Patch, Post } from '@nestjs/common'
 
 @Controller('mocks')
-export class MocksController {}
+export class MocksController {
+  @Post('enqueues')
+  enqueue() {
+    return
+  }
+
+  @Post('seats/:seat_id/reservations')
+  reserve() {
+    return
+  }
+
+  @Get('seats')
+  getSeats() {
+    return
+  }
+
+  @Patch('charges')
+  charge() {
+    return
+  }
+
+  @Get('charges')
+  getCharges() {
+    return
+  }
+
+  @Post('payments')
+  pay() {
+    return
+  }
+}
