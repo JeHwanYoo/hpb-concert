e79f1376b327b4dc9358a23d8b94b08a030e3a3b
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2024-04-09 14:51:17 +0900

    [#3] test: mock repository

--- src/domains/users/users.service.spec.ts
+++ src/domains/users/users.service.spec.ts
@@ -1,23 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing'
-import { beforeEach, describe, expect, it } from 'vitest'
+import { afterEach, beforeEach, describe, expect, it, Mock, vi } from 'vitest'
 import { UsersService } from './users.service'
 import { UsersRepositoryToken } from './users.repository'
 
 describe('UsersService', () => {
   let service: UsersService
+  let mockRepository: Mock
 
   beforeEach(async () => {
+    mockRepository = vi.fn()
+
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         UsersService,
         {
           provide: UsersRepositoryToken,
-          useValue: {},
+          useValue: mockRepository,
         },
       ],
     }).compile()
 
     service = module.get<UsersService>(UsersService)
+  })
+
+  afterEach(() => {
+    vi.resetAllMocks()
   })
 
   it('should be defined', () => {
