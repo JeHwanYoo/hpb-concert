27a32d8bdf9eee84ee7350202d38c4eb186a28ff
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2024-04-09 13:43:42 +0900

    [#3] feat: define user repository interface

--- src/domains/users/users.repository.ts
+++ src/domains/users/users.repository.ts
@@ -1,3 +1,16 @@
+import { UserModel } from './models/user.model'
+import { UserMutationModel } from './models/user.mutation.model'
+
 export const UsersRepositoryToken = 'UsersRepository'
 
-export interface UsersRepository {}
+export interface UsersRepository {
+  create(mutationModel: UserMutationModel): string
+
+  findOne(): UserModel
+
+  findAll(): UserModel[]
+
+  update(id: string, mutationModel: UserMutationModel): string
+
+  remove(id: string): string
+}
