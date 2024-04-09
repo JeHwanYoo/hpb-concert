1eed2786d6354bbac3a4feeec7d989e91bd96c87
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2024-04-09 14:25:53 +0900

    [#3] docs: UserRepository

--- src/domains/users/users.repository.ts
+++ src/domains/users/users.repository.ts
@@ -8,15 +8,41 @@ import {
 export const UsersRepositoryToken = 'UsersRepository'
 
 export interface UsersRepository {
+  /**
+   *
+   * @param mutationModel to create
+   * @returns The UUID that was created
+   */
   create(mutationModel: UserMutationModel): Promise<string>
 
+  /**
+   *
+   * @param id User's id
+   * @returns The UserModel that matched
+   */
   findOne(id: string): Promise<UserModel>
 
+  /**
+   *
+   * @param query
+   * @returns The paginated result of the query
+   */
   find(
     query: OffsetBasedPaginationQuery,
   ): Promise<OffsetBasedPaginationResult<UserModel>>
 
+  /**
+   *
+   * @param id User's id
+   * @param mutationModel to update
+   * @returns The UUID that was updated
+   */
   update(id: string, mutationModel: UserMutationModel): Promise<string>
 
+  /**
+   *
+   * @params User's id
+   * @returns The UUID that was removed
+   */
   remove(id: string): Promise<string>
 }
