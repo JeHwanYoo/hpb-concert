3405d10432c0597fbed090c9ed0a8e167e325058
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2024-04-11 15:45:58 +0900

    [#4] infra: set swagger

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -12,6 +13,15 @@ async function bootstrap() {
       logger: true,
     }),
   )
+
+  const config = new DocumentBuilder()
+    .setTitle('콘서트 예약')
+    .setDescription('콘서트 예약 시나리오 구현')
+    .setVersion('1.0')
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('docs', app, document)
+
   await app.listen(3000, '0.0.0.0')
 }
 
