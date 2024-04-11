24a8329c4bd86b74c608d6fcad2fe8783e6e187e
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2024-04-11 11:51:22 +0900

    [#4] refactor: test with testcontainers

--- src/infra/repositories/users/users.prisma.repository.spec.ts
+++ src/infra/repositories/users/users.prisma.repository.spec.ts
@@ -3,22 +3,53 @@ import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
 import { UsersPrismaRepository } from './users.prisma.repository'
 import { PrismaModule } from '../../prisma/prisma.module'
 import { PrismaService } from '../../prisma/prisma.service'
+import { GenericContainer } from 'testcontainers'
+import { execSync } from 'child_process'
 
 describe('UsersPrismaRepository', () => {
   let repository: UsersPrismaRepository
   let prisma: PrismaService
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      imports: [PrismaModule],
-      providers: [UsersPrismaRepository],
-    }).compile()
-    await module.init()
+  // Prepare a TestContainer for setting up a PostgreSQL instance.
+  beforeEach(
+    async () => {
+      // Initialize the container
+      console.log('Initialize PG Container ...')
+      const container = await new GenericContainer('postgres:latest')
+        .withExposedPorts(5432)
+        .withEnvironment({
+          POSTGRES_USER: 'user',
+          POSTGRES_PASSWORD: 'password',
+          POSTGRES_DB: 'test',
+        })
+        .start()
 
-    repository = module.get<UsersPrismaRepository>(UsersPrismaRepository)
+      // Get the mappedPort from the container and Set the DATABASE_URL
+      const mappedPort = container.getMappedPort(5432)
+      process.env.DATABASE_URL = `postgresql://user:password@localhost:${mappedPort}/test?schema=public`
 
-    prisma = new PrismaService()
-  })
+      // Migrate TestContainer and PostgreSQL image
+      execSync('npx prisma migrate dev', {
+        env: {
+          ...process.env,
+          DATABASE_URL: process.env.DATABASE_URL,
+        },
+      })
+
+      prisma = new PrismaService()
+
+      const module: TestingModule = await Test.createTestingModule({
+        imports: [PrismaModule],
+        providers: [UsersPrismaRepository],
+      }).compile()
+      await module.init()
+
+      repository = module.get<UsersPrismaRepository>(UsersPrismaRepository)
+
+      console.log('Initialize PG Container Done')
+    },
+    1000 * 60 * 3,
+  )
 
   afterEach(() => {
     vi.resetAllMocks()
@@ -36,6 +67,8 @@ describe('UsersPrismaRepository', () => {
           id: createdUser.id,
         },
       })
+
+      expect(expectedUser).to.be.a('object')
       expect(expectedUser.id).to.be.eq(createdUser.id)
       expect(expectedUser.name).to.be.eq(createdUser.name)
       expect(expectedUser.createdAt).to.be.deep.eq(createdUser.createdAt)
