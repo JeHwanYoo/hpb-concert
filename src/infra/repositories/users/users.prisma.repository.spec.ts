c5643e467eea511d5b0ee436ef709c4a6a9fcb75
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2024-04-11 13:16:56 +0900

    [#4] feat: .findOne() should find a user

--- src/infra/repositories/users/users.prisma.repository.spec.ts
+++ src/infra/repositories/users/users.prisma.repository.spec.ts
@@ -1,16 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing'
-import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
+import { afterEach, beforeAll, describe, expect, it, vi } from 'vitest'
 import { UsersPrismaRepository } from './users.prisma.repository'
 import { PrismaModule } from '../../prisma/prisma.module'
 import { PrismaService } from '../../prisma/prisma.service'
 import { setUpIntegratedTest } from '../../../shared/integrated.test.setup'
+import { UserModel } from '../../../domains/users/models/user.model'
 
 describe('UsersPrismaRepository', () => {
   let repository: UsersPrismaRepository
   let prisma: PrismaService
 
   // Prepare a TestContainer for setting up a PostgreSQL instance.
-  beforeEach(
+  beforeAll(
     () =>
       setUpIntegratedTest(async _prisma => {
         prisma = _prisma
@@ -26,8 +27,9 @@ describe('UsersPrismaRepository', () => {
     1000 * 60 * 3,
   )
 
-  afterEach(() => {
-    vi.resetAllMocks()
+  // Initialize databases to ensure test idempotency
+  afterEach(async () => {
+    await prisma.user.deleteMany()
   })
 
   it('should be defined', async () => {
@@ -36,18 +38,32 @@ describe('UsersPrismaRepository', () => {
 
   describe('.create()', () => {
     it('should create a user', async () => {
-      const createdUser = await repository.create({ name: 'John' })
-      const expectedUser = await prisma.user.findUnique({
+      const expectedUser = await repository.create({ name: 'John' })
+      const foundUser = await prisma.user.findUnique({
         where: {
-          id: createdUser.id,
+          id: expectedUser.id,
         },
       })
 
-      expect(expectedUser).to.be.a('object')
-      expect(expectedUser.id).to.be.eq(createdUser.id)
-      expect(expectedUser.name).to.be.eq(createdUser.name)
-      expect(expectedUser.createdAt).to.be.deep.eq(createdUser.createdAt)
-      expect(expectedUser.updatedAt).to.be.deep.eq(createdUser.updatedAt)
+      assertUser(expectedUser, foundUser)
+    })
+  })
+
+  describe('.findOne()', () => {
+    it('should find a user', async () => {
+      const createdUser = await prisma.user.create({
+        data: {
+          name: 'John',
+        },
+      })
+      const expectedUser = await repository.findOne(createdUser.id)
+
+      assertUser(expectedUser, createdUser)
     })
   })
 })
+
+function assertUser(expected: UserModel, wanted: UserModel) {
+  expect(expected).to.be.a('object')
+  expect(expected).to.be.deep.eq(wanted)
+}
