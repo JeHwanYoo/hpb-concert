66577bfe3aec8347305e4e88e9e249d635c5910a
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2024-04-11 17:46:39 +0900

    [#4] feat: EnqueuesService.createToken

--- src/domains/enqueues/enqueues.service.spec.ts
+++ src/domains/enqueues/enqueues.service.spec.ts
@@ -2,22 +2,34 @@ import { beforeEach, describe, expect, it, vi } from 'vitest'
 import { Test, TestingModule } from '@nestjs/testing'
 import { EnqueuesService } from './enqueues.service'
 import Redis from 'ioredis'
-import { validate as validateUUID } from 'uuid'
 import { ConfigModule } from '@nestjs/config'
-import { JwtModule } from '@nestjs/jwt'
+import { JwtModule, JwtService } from '@nestjs/jwt'
+import {
+  DEFAULT_REDIS_NAMESPACE,
+  getRedisToken,
+} from '@liaoliaots/nestjs-redis'
+import { TokenModel } from './models/token.model'
 
 describe('EnqueuesService', () => {
   let service: EnqueuesService
   let mockRedis: Redis
+  let jwtService: JwtService
 
   beforeEach(async () => {
     mockRedis = {} as Redis
     const module: TestingModule = await Test.createTestingModule({
       imports: [ConfigModule.forRoot(), JwtModule.register({ secret: 'test' })],
-      providers: [EnqueuesService],
+      providers: [
+        EnqueuesService,
+        {
+          provide: getRedisToken(DEFAULT_REDIS_NAMESPACE),
+          useValue: mockRedis,
+        },
+      ],
     }).compile()
 
     service = module.get<EnqueuesService>(EnqueuesService)
+    jwtService = module.get<JwtService>(JwtService)
   })
 
   it('should be defined', () => {
@@ -29,10 +41,22 @@ describe('EnqueuesService', () => {
       mockRedis.get = vi.fn().mockResolvedValue('0')
       mockRedis.incr = vi.fn().mockResolvedValue(1)
 
-      const token = await service.createToken()
+      const token = await service.createToken('fake-user-id')
+
+      console.log(token)
 
       expect(token).to.be.a('string')
-      expect(validateUUID(token)).to.be.true
+
+      const verified = jwtService.verify<TokenModel>(token, { secret: 'test' })
+      console.log(verified)
+
+      expect(verified).to.be.a('object')
+      expect(verified.userId).to.be.string('fake-user-id')
+      expect(verified.availableTime).to.be.a('number')
+      expect(verified.exp).to.be.a('number')
+      expect(verified.exp - verified.availableTime).to.be.greaterThanOrEqual(
+        5 * 60,
+      )
     })
   })
 })
---
+++ src/domains/enqueues/models/token.model.ts
@@ -0,0 +1,8 @@
+export interface TokenModel {
+  // requested userId
+  userId: string
+  // RFC 7519 (seconds)
+  availableTime: number
+  // RFC 7519 (seconds)
+  exp: number
+}
