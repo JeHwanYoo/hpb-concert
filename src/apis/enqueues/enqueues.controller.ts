98830bef3ce3d9cc5d83d10c3e3fa75e2ef4b7c8
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2024-04-11 16:07:57 +0900

    [#4] docs: commonize token

--- src/apis/enqueues/enqueues.controller.ts
+++ src/apis/enqueues/enqueues.controller.ts
@@ -5,10 +5,16 @@ import {
   ApiOperation,
   ApiTags,
 } from '@nestjs/swagger'
+import {
+  EnqueueTokenExampleValue,
+  UserTokenExampleValue,
+} from '../../shared/share.openapi'
 
 @Controller('v1/enqueues')
 @ApiTags('Enqueues')
 export class EnqueuesController {
+  constructor() {}
+
   @Post()
   @ApiOperation({
     description: '대기열 토큰 발급 API',
@@ -18,8 +24,7 @@ export class EnqueuesController {
     description: 'User Bearer token (JWT)',
     schema: {
       type: 'string',
-      example:
-        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIwYjIzOTQ4OC00ZmE0LTQzMzQtYmE3MC04YTZmYmU4MDU5MmMiLCJleHBpcmVkX2F0IjoiMjAyNC0wNi0xMlQyMzozNjoxNS4zNTRaIiwiY3JlYXRlZF9hdCI6IjIwMjQtMDQtMDRUMTI6NTc6MTUuMzU0WiIsImlhdCI6MTcxMjIzNTQzNX0.Gywvr6ZpT0tPhgqrpTLB3pfu36rh_eNmZWF0rtRUUDk',
+      example: `Bearer ${UserTokenExampleValue}`,
     },
   })
   @ApiCreatedResponse({
@@ -27,8 +32,7 @@ export class EnqueuesController {
       'Enqueue Bearer token (JWT)<br/>토큰은 `입장 가능 시간`, `만료 시간`을 포함하고 있음',
     schema: {
       type: 'string',
-      example:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIwYjIzOTQ4OC00ZmE0LTQzMzQtYmE3MC04YTZmYmU4MDU5MmMiLCJleHBpcmVkX2F0IjoiMjAyNC0wNi0xMlQyMzozODoyNC44MDhaIiwiY3JlYXRlZF9hdCI6IjIwMjQtMDQtMDRUMTI6NTk6MjQuODA4WiIsImlhdCI6MTcxMjIzNTU2NH0.2OHmzbGoHoA6PTeW5_g0LkMgQavOWtt9reryAPmXuV4',
+      example: EnqueueTokenExampleValue,
     },
   })
   enqueues(): string {
