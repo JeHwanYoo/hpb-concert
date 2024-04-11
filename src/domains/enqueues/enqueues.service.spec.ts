3b732c7d9276af08eb771d121f62e1ba7df845e1
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2024-04-11 17:00:11 +0900

    [#4] test: .createToken() should create a token

--- src/domains/enqueues/enqueues.service.spec.ts
+++ src/domains/enqueues/enqueues.service.spec.ts
@@ -1,7 +1,8 @@
-import { beforeEach, describe, expect, it } from 'vitest'
+import { beforeEach, describe, expect, it, vi } from 'vitest'
 import { Test, TestingModule } from '@nestjs/testing'
 import { EnqueuesService } from './enqueues.service'
 import Redis from 'ioredis'
+import { validate as validateUUID } from 'uuid'
 
 describe('EnqueuesService', () => {
   let service: EnqueuesService
@@ -18,5 +19,17 @@ describe('EnqueuesService', () => {
 
   it('should be defined', () => {
     expect(service).to.be.a('object')
+  })
+
+  describe('.createToken()', () => {
+    it('should create a token', async () => {
+      mockRedis.get = vi.fn().mockResolvedValue(0)
+      mockRedis.incr = vi.fn().mockResolvedValue(1)
+
+      const token = await service.createToken()
+
+      expect(token).to.be.a('string')
+      expect(validateUUID(token)).to.be.true
+    })
   })
 })
