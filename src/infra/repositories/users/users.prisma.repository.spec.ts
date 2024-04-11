47c4972d0c72838d42fb13df514dbf73a5fd4a37
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2024-04-11 10:16:05 +0900

    [#4] test: .create() should create a user

--- src/infra/repositories/users/users.prisma.repository.spec.ts
+++ src/infra/repositories/users/users.prisma.repository.spec.ts
@@ -2,9 +2,11 @@ import { Test, TestingModule } from '@nestjs/testing'
 import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
 import { UsersPrismaRepository } from './users.prisma.repository'
 import { PrismaModule } from '../../prisma/prisma.module'
+import { PrismaService } from '../../prisma/prisma.service'
 
 describe('UsersPrismaRepository', () => {
   let repository: UsersPrismaRepository
+  let prisma: PrismaService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -14,6 +16,8 @@ describe('UsersPrismaRepository', () => {
     await module.init()
 
     repository = module.get<UsersPrismaRepository>(UsersPrismaRepository)
+
+    prisma = new PrismaService()
   })
 
   afterEach(() => {
@@ -22,5 +26,20 @@ describe('UsersPrismaRepository', () => {
 
   it('should be defined', async () => {
     expect(repository).to.not.be.undefined
+  })
+
+  describe('.create()', () => {
+    it('should create a user', async () => {
+      const createdUser = await repository.create({ name: 'John' })
+      const expectedUser = await prisma.user.findUnique({
+        where: {
+          id: createdUser.id,
+        },
+      })
+      expect(expectedUser.id).to.be.eq(createdUser.id)
+      expect(expectedUser.name).to.be.eq(createdUser.name)
+      expect(expectedUser.createdAt).to.be.deep.eq(createdUser.createdAt)
+      expect(expectedUser.updatedAt).to.be.deep.eq(createdUser.updatedAt)
+    })
   })
 })
