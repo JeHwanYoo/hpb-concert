e64e9888f42a379f23ca12a2dce71dd33ae4395f
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2024-04-11 22:45:02 +0900

    [#4] docs: remove mocks

--- src/mocks/mocks.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common'
-import { MocksController } from './mocks.controller'
-import { JwtModule } from '@nestjs/jwt'
-
-@Module({
-  imports: [JwtModule.register({ secret: 'mock' })],
-  controllers: [MocksController],
-})
-export class MocksModule {}
