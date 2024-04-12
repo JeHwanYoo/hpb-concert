961730cbb20fb3653cf1442cf5ef16629fdea8d5
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2024-04-12 12:02:40 +0900

    [#4] feat: EnqueueTokensGuard

--- src/domains/tokens/tokens.guard.ts
+++ src/domains/tokens/tokens.guard.ts
@@ -29,3 +29,12 @@ export class UserTokensGuard implements CanActivate {
     }
   }
 }
+
+/**
+ * @todo implement
+ */
+export class EnqueueTokensGuard implements CanActivate {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    return true
+  }
+}
