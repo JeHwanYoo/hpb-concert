15cfcaf7c30f0a4b5d96646b9f02530ae3de3ccd
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2024-04-12 11:35:50 +0900

    [#4] feat: Concerts Dynamic module

--- src/domains/concerts/concerts.module.ts
+++ src/domains/concerts/concerts.module.ts
@@ -1,4 +1,26 @@
-import { Module } from '@nestjs/common'
+import { DynamicModule, Module, Provider } from '@nestjs/common'
+import {
+  ConcertsRepository,
+  ConcertsRepositoryToken,
+} from './concerts.repository'
+import { ConcertsService } from './concerts.service'
+
+export interface ConcertsModuleProps {
+  ConcertsRepository: new (...args: unknown[]) => ConcertsRepository
+}
 
 @Module({})
-export class ConcertsModule {}
+export class ConcertsModule {
+  static forFeature(props: ConcertsModuleProps): DynamicModule {
+    const dynamicUsersRepositoryProvider: Provider = {
+      provide: ConcertsRepositoryToken,
+      useClass: props.ConcertsRepository,
+    }
+
+    return {
+      module: ConcertsModule,
+      providers: [ConcertsService, dynamicUsersRepositoryProvider],
+      exports: [ConcertsService],
+    }
+  }
+}
