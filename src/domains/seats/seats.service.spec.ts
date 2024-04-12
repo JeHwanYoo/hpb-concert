8c113a06ef9e599ea7b20fc9f5eb4b17f3ada672
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2024-04-12 15:51:25 +0900

    [#4] test: should reserve a seat and it was already reserved but the deadline exceeds

--- src/domains/seats/seats.service.spec.ts
+++ src/domains/seats/seats.service.spec.ts
@@ -80,9 +80,37 @@ describe('SeatsService', () => {
         }),
       ).rejects.toThrow('Already reserved')
     })
-    it.todo(
-      'should reserve a seat and it was already reserved but the deadline exceeds',
-    )
+    it('should reserve a seat and it was already reserved but the deadline exceeds', async () => {
+      const past = faker.date.past({ refDate: new Date() })
+      mockRepository.findOneBySeatNo = vi.fn().mockResolvedValue({
+        reservedAt: past,
+        deadline: addMinutes(past, 5),
+      })
+
+      mockRepository.create = vi.fn().mockImplementation(param => ({
+        ...param,
+        id: v4(),
+      }))
+
+      const reserved = await service.reserve({
+        holderId: 'fake-id',
+        concertId: 'fake-id',
+        seatNo: 0,
+      })
+
+      expect(reserved).to.have.keys(
+        'id',
+        'holderId',
+        'concertId',
+        'seatNo',
+        'reservedAt',
+        'deadline',
+      )
+      expect(
+        differenceInMinutes(reserved.deadline, reserved.reservedAt),
+      ).to.be.greaterThanOrEqual(5)
+      expect(reserved.reservedAt).to.be.greaterThan(past)
+    })
     it.todo('should not reserve a seat if it was already paid')
   })
 
