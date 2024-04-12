8c6a64a9b7869e75e8332a8bace6be592494f36e
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2024-04-12 13:19:26 +0900

    [#4] feat: BillService

--- src/domains/bills/bills.service.ts
+++ src/domains/bills/bills.service.ts
@@ -1,5 +1,6 @@
 import { Inject, Injectable } from '@nestjs/common'
 import { BillsRepository, BillsRepositoryToken } from './bills.repository'
+import { BillCreationModel } from './models/bill.model'
 
 @Injectable()
 export class BillsService {
@@ -7,4 +8,12 @@ export class BillsService {
     @Inject(BillsRepositoryToken)
     private readonly billsRepository: BillsRepository,
   ) {}
+
+  create(createModel: BillCreationModel) {
+    return this.billsRepository.create(createModel)
+  }
+
+  findByUserId(userId: string) {
+    return this.billsRepository.findOneByUserId(userId)
+  }
 }
