43566adb5a329ada4b37a120981cf3dcce855833
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2024-04-18 16:22:20 +0900

    [#6] feat(repository): ChargesPrismaRepository.create()

--- src/infra/repositories/charges/charges.prisma.repository.ts
+++ src/infra/repositories/charges/charges.prisma.repository.ts
@@ -6,12 +6,16 @@ import {
   ChargeUpdatingModel,
 } from '../../../domains/charges/models/charge.model'
 import { PrismaService } from '../../prisma/prisma.service'
+import { Injectable } from '@nestjs/common'
 
+@Injectable()
 export class ChargesPrismaRepository implements ChargesRepository {
   constructor(private readonly prisma: PrismaService) {}
 
   create(creationModel: ChargeCreationModel): Promise<ChargeModel> {
-    return Promise.resolve(undefined)
+    return this.prisma.charge.create({
+      data: creationModel,
+    })
   }
 
   findOneBy(by: IdentifierFrom<ChargeModel>): Promise<ChargeModel> {
