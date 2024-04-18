202e4ec6ff6cc1aace83988987339694b5b78742
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2024-04-18 23:48:13 +0900

    [#6] fix(repository): user repository

--- src/domains/users/users.repository.ts
+++ src/domains/users/users.repository.ts
@@ -1,65 +1,38 @@
-import {
-  UserCreationModel,
-  UserModel,
-  UserUpdatingModel,
-} from './models/user.model'
+import { UserCreationModel, UserModel } from './models/user.model'
 import {
   OffsetBasedPaginationQuery,
   OffsetBasedPaginationResult,
 } from '../../shared/shared.dto'
 import { IdentifierFrom } from '../../shared/shared.type.helper'
+import { TransactionalOperation } from '../../shared/transaction/transaction.service'
 
 export const UsersRepositoryToken = 'UsersRepository'
 
-export interface UsersRepository<S = unknown> {
+export interface UsersRepository<Connection = unknown> {
   /**
    *
    * @param createModel
-   * @param session
    * @returns The UUID that was created
    */
-  create(createModel: UserCreationModel, session?: S): Promise<UserModel>
+  create(
+    createModel: UserCreationModel,
+  ): TransactionalOperation<UserModel, Connection>
 
   /**
    *
    * @param by
-   * @param session
    * @returns The UserModel that matched
    */
   findOneBy(
     by: IdentifierFrom<UserModel>,
-    session?: S,
-  ): Promise<UserModel | null>
+  ): TransactionalOperation<UserModel | null, Connection>
 
   /**
    *
    * @param by
-   * @param session
    * @returns The paginated result of the query
    */
   findManyBy(
     by: Partial<UserModel> & OffsetBasedPaginationQuery,
-    session?: S,
-  ): Promise<OffsetBasedPaginationResult<UserModel>>
-
-  /**
-   *
-   * @param id User's id
-   * @param updatingModel
-   * @param session
-   * @returns The UUID that was updated
-   */
-  update(
-    id: string,
-    updatingModel: UserUpdatingModel,
-    session?: S,
-  ): Promise<UserModel | null>
-
-  /**
-   *
-   * @param id User's id
-   * @param session
-   * @returns The UUID that was removed
-   */
-  remove(id: string, session?: S): Promise<string>
+  ): TransactionalOperation<OffsetBasedPaginationResult<UserModel>, Connection>
 }
