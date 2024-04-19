d7602addd92f5ac173ce3fc44efbd0c82da424cf
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2024-04-19 16:57:25 +0900

    [#6] feat(domain): EnqueueTokensGuard

--- src/domains/tokens/tokens.guard.ts
+++ src/domains/tokens/tokens.guard.ts
@@ -1,10 +1,13 @@
 import {
+  BadRequestException,
   CanActivate,
   ExecutionContext,
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
+import { EnqueueTokenModel } from './models/enqueueTokenModel'
+import { differenceInSeconds } from 'date-fns'
 
 @Injectable()
 export class UserTokensGuard implements CanActivate {
@@ -30,11 +33,37 @@ export class UserTokensGuard implements CanActivate {
   }
 }
 
-/**
- * @todo implement
- */
+@Injectable()
 export class EnqueueTokensGuard implements CanActivate {
+  constructor(private readonly jwtService: JwtService) {}
+
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest()
+
+    const authHeader = request.headers['authorization']
+
+    if (!authHeader) throw new UnauthorizedException()
+
+    if (!authHeader.startsWith('Bearer ')) throw new UnauthorizedException()
+
+    const token = authHeader.split(' ')[1]
+
+    let verified: EnqueueTokenModel
+    try {
+      verified = await this.jwtService.verifyAsync<EnqueueTokenModel>(token)
+    } catch (e) {
+      throw new UnauthorizedException(e.message)
+    }
+
+    const now = new Date()
+    const availableDate = new Date(verified.availableTime * 1000)
+    if (differenceInSeconds(now, availableDate) < 0) {
+      throw new BadRequestException('Insufficient Entry Time.')
+    }
+    if (verified.completed) {
+      throw new BadRequestException('Session Completed')
+    }
+
     return true
   }
 }
