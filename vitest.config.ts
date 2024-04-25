51891cece41c79babfd093edd3412e46d51dd70a
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2024-04-25 22:44:43 +0900

    chore: setting for coverage test

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,13 @@ export default defineConfig({
   test: {
     globals: true,
     root: './',
+    coverage: {
+      provider: 'v8',
+      // you can include other reporters, but 'json-summary' is required, json is recommended
+      reporter: ['text', 'json-summary', 'json'],
+      // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
+      reportOnFailure: true,
+    },
   },
   plugins: [
     swc.vite({
