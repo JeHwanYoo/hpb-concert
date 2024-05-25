a680e3d60af832ee92fd5023d63bc4a8990fcfa4
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2024-05-25 12:30:35 +0900

    refactor: todo pub/sub

--- src/infra/redis/redis.lock.service.ts
+++ src/infra/redis/redis.lock.service.ts
@@ -3,6 +3,7 @@ import Redis from 'ioredis'
 import { LockService } from '../../shared/lock/lock.service'
 import { InjectRedis } from '@nestjs-modules/ioredis'
 
+// @todo refactor pub/sub
 @Injectable()
 export class RedisDistributedLockService implements LockService {
   constructor(@InjectRedis() private readonly redis: Redis) {}
